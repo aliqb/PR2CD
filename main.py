536170672bfafebbb6d4c1d592409fe7bde2fe57
Ali Qayyoomi <aliqbhastam@gmail.com>
2024-03-28 13:56:49 +0330

    tagger and parser dependencies

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from hazm.dependency_parser import SpacyDependencyParser
+from hazm import POSTagger, Lemmatizer, DependencyParser
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def printGraph(dg):
+    for head, rel, dep in dg.triples():
+        # print('({h[0]}, {h[1]}), {r}, ({d[0]}, {d[1]})'.format(h=head, r=rel, d=dep))
+
+        print(f'head:{head[0]}, {head[1]}')
+        print(rel)
+        print(f'dep:{dep[0]}, {dep[1]}')
+        print('-----------------------------------')
+
+
+if __name__ == '__main__':
+    parser = DependencyParser(tagger=POSTagger(model='pos_tagger.model'), lemmatizer=Lemmatizer())
+    dg = parser.parse(['من', 'به', 'مدرسه', 'رفته بودم', '.'])
+    dg.tree().pprint()
+    printGraph(dg)
+    # tagger = POSTagger(model='pos_tagger.model')
+    # lemmatizer = Lemmatizer()
+    # spacy_parser = SpacyDependencyParser(tagger=tagger, lemmatizer=lemmatizer,
+    #                                      model_file='./spacy_dependency_parser/parser/model',
+    #                                      working_dir='./spacy_dependency_parser')
+    dg1 = parser.parse_sents(sentences='من به مدرسته رفته بودم.')
+    print(dg1)
+    # spacy_parser = SpacyDependencyParser(tagger=POSTagger(model='pos_tagger.model'), lemmatizer=Lemmatizer())
+
