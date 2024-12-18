025545ba76fb9b5b563b97afd7183ab09d3470a2
Ali Qayyoomi <aliqbhastam@gmail.com>
2024-12-18 22:54:54 +0330

    threads

--- PR2CD_UI/runserver.py
+++ PR2CD_UI/runserver.py
@@ -4,4 +4,4 @@ from PR2CD_UI.wsgi import application
 # documentation: https://docs.pylonsproject.org/projects/waitress/en/stable/api.html
 
 if __name__ == '__main__':
-    serve(application, host = 'localhost', port='8080')
+    serve(application, host = 'localhost', port='8080',threads=8)
